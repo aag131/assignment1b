da2970cad67b9b9b34dc73587c76cf90799a7bca
Alyssa Goodwin <aag131@uw.edu>
2023-11-04 20:52:57 -0700

    Wrote DropOutStack w/o toString*Untested

--- DropOutStack.java
+++ DropOutStack.java
@@ -1,18 +1,60 @@
 public class DropOutStack<T> {
-    public DropOutStack(int capacity) { /* TODO write stack constructor */ }
+    T[] arr;
+    int top;
+    public DropOutStack(int capacity) {
+        arr = (T[]) new Object[capacity];
+        top = -1;
+    }
 
-    public void push(T elem) { /* TODO write push method */ }
+    public void push(T elem) {
+        // check for overflow and if true assigns top to the beginning of the array
+        if(isFull()) {
+            top = 0;
+            arr[top] = elem;
+        }
+        // assigns top to the next index and will overwrite the bottom of the stack
+        // if the stack is at capacity
+        top++;
+        arr[top] = elem;
+    }
 
     public T pop() { /* TODO write pop method */
-        return null;
+        // check for underflow and throw exception
+        if (isEmpty()) {
+            throw new IllegalStateException("Cannot call pop() on empty stack");
+        }
+        T temp = arr[top];
+        top--;
+        return temp;
     }
 
     public T peek() { /* TODO write peek method */
-        return null;
+        // check for underflow and throw exception
+        if (isEmpty()) {
+            throw new IllegalStateException("Cannot call pop() on empty stack");
+        }
+        return arr[top];
     }
 
     @Override
     public String toString() { // TODO write toString method
         return "";
     }
+
+    //private helper method to check for underflow
+    private boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        }
+        return false;
+    }
+    // private helper method to check for overflow. In drop out stack imlementation,
+    // it mostly just checks if the top is at the end of the array
+    private boolean isFull() {
+        if (top==arr.length-1) {
+            return true;
+        }
+        return false;
+    }
+
 }
